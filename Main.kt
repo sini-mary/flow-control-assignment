f9d93833cd9a1e80cde8c2e2db2788ed1354d8fa
sini-mary <marysnaidaa@gmail.com>
2023-02-16 08:54:33 +0300

    strings

---
+++ Main.kt
@@ -0,0 +1,24 @@
+import kotlin.concurrent.fixedRateTimer
+
+fun main() {
+var Institute = "akirachix"
+  println(Institute[1])
+  println(Institute[3])
+  println(Institute[4])
+
+
+}
+fun sentence(name: String , age : Int) :String{
+  val nameAge = "Hello, my name is $name and I am $age years old"
+  return (nameAge)
+}
+fun display(person: String): Int {
+    val Find ="Sansa"
+  return (Find.length)
+}
+fun identity(name: String ) = if (name=="Snaida") {
+  println("That's  me")
+}
+else {
+  println("I don't know who that is")
+}
